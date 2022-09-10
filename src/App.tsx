1e76b247e671362841a496e7ef580036122aff9e
sakthikaaylabs <sakthivel.arumugapandian@kaaylabs.com>
2022-09-10 21:07:53 +0530

    PWA setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Academic web PWA Trails
         </a>
       </header>
     </div>
